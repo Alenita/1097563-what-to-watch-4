dd0924299772b7941f435fa5c6a1e2c45868ba8d
Alena Gorshkova <alenagorsh@gmail.com>
2020-06-11 22:47:30 +0300

    start programming

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
+
+const filmInfo = {
+  filmName: `The Green Mile`,
+  genre: `Drama`,
+  filmRelease: `1999`
+};
+
+ReactDOM.render(
+    <App name={filmInfo.filmName} genre={filmInfo.genre} date={filmInfo.filmRelease} />, document.querySelector(`#root`)
+);
